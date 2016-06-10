975c0c3dbd77bba8fe51f9671a8c4aa4b88f5ea1
qxy <18735359200@163.com>
2016-06-10 17:50:47 +0800

    add Xib DS CO Community

---
+++ Controller/Community/XYAddCommunityDSCOViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYAddCommunityDSCOViewController.h
+//  zhaoZhaoBa
+//
+//  Created by apple on 16/6/10.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import "XYRootViewController.h"
+
+@interface XYAddCommunityDSCOViewController : XYRootViewController
+
+@end
